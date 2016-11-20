5050cc7047591747a96a53b11a158242148dbf85
Arif Khan <arifkhan.tech@gmail.com>
2016-11-20 09:40:06 +0530

    second commit

--- accounting software/checkProductIdInInventory.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once 'dbconfig.php';
-$stmt34=$DB_con->prepare("select * from inventory where p_id=:p_id");
-$stmt34->bindValue(":p_id", "%".$_GET["q"]."%");
-$stmt34->execute();
-while ( $row = $stmt34->fetchObject() ) {
-	echo $row->p_name;
-}
